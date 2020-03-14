bfa52ff9ce5062fdb111df3e6554ddc31b926d47
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2020-03-14 19:37:02 -0400

    Start player and enemy model

--- core/src/com/fallenflame/game/EnemyModel.java
+++ core/src/com/fallenflame/game/EnemyModel.java
@@ -1,5 +1,52 @@
 package com.fallenflame.game;
 
-public class EnemyModel {
-    //TODO
+import com.badlogic.gdx.math.Vector2;
+
+public class EnemyModel extends PlayerModel {
+    protected boolean activated;
+
+    protected Vector2 playerLastKnown = new Vector2();
+
+    @Override
+    protected float defaultMaxSpeed() {
+        return super.defaultMaxSpeed() * 1.25f;
+    }
+
+    @Override
+    protected String defaultTexture() {
+        return "enemy-walking";
+    }
+
+    public boolean getActivated() {
+        return this.activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = true;
+    }
+
+    public Vector2 getPlayerLastKnown() {
+        return playerLastKnown.cpy();
+    }
+
+    public float getPlayerLastKnownX() {
+        return playerLastKnown.x;
+    }
+
+    public float getPlayerLastKnownY() {
+        return playerLastKnown.y;
+    }
+
+    public void setPlayerLastKnown(Vector2 v) {
+        playerLastKnown.set(v);
+    }
+
+    public void setPlayerLastKnown(float x, float y) {
+        this.setPlayerLastKnown(new Vector2(x, y));
+    }
+
+    @Override
+    public float getLightRadius() {
+        return getActivated() ? 1 : 0;
+    }
 }
