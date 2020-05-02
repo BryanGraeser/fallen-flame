fe7664996a2f8c5083b25df86b4a4b13f14ff17c
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2020-05-03 04:46:25 +0800

    Pref for key rebind now saves

--- core/src/com/fallenflame/game/util/InputBindings.java
+++ core/src/com/fallenflame/game/util/InputBindings.java
@@ -1,11 +1,14 @@
 package com.fallenflame.game.util;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class InputBindings {
+    private static final Preferences prefs = Gdx.app.getPreferences("fallen_flame_key_binding");
     public enum Control {
         GO_UP,
         GO_DOWN,
@@ -20,7 +23,8 @@ public class InputBindings {
     private static final Map<Control, Integer> bindings;
     static {
         bindings = new HashMap<>();
-        reset();
+        reset(false);
+        readFromPrefs();
     }
     public static int getBindingOf(Control c) {
         return bindings.get(c);
@@ -75,6 +79,14 @@ public class InputBindings {
                 return false;
         }
     }
+    private static void readFromPrefs() {
+        for (Control c : Control.values()) {
+            String id = controlToID(c);
+            if (prefs.contains(id)) {
+                setBindingOfWithoutCheck(c, prefs.getInteger(id));
+            }
+        }
+    }
     private static void switchControlIfNeeded(Control c, int i) {
         Control con = null;
         for (Map.Entry<Control, Integer> c2 : bindings.entrySet()) {
@@ -88,7 +100,7 @@ public class InputBindings {
             bindings.put(con, getBindingOf(c));
         }
     }
-    public static void reset() {
+    public static void reset(boolean resetPrefs) {
         bindings.put(Control.GO_UP, Input.Keys.W);
         bindings.put(Control.GO_DOWN, Input.Keys.S);
         bindings.put(Control.GO_LEFT, Input.Keys.A);
@@ -98,6 +110,13 @@ public class InputBindings {
         bindings.put(Control.SPRINTING, Input.Keys.SHIFT_LEFT);
         bindings.put(Control.INCREASE_LIGHT, Input.Keys.PERIOD);
         bindings.put(Control.DECREASE_LIGHT, Input.Keys.COMMA);
+        if (resetPrefs) {
+            prefs.clear();
+            prefs.flush();
+        }
+    }
+    public static void reset() {
+        reset(true);
     }
     public static String keyToString(int k) {
         switch(k) {
@@ -112,10 +131,44 @@ public class InputBindings {
                 return Input.Keys.toString(k);
         }
     }
+    private static void setBindingOfWithoutCheck(Control c, int i) {
+        if (!allowed(i)) return;
+        bindings.put(c, i);
+    }
     public static void setBindingOf(Control c, int i) {
         if (!allowed(i)) return;
         switchControlIfNeeded(c, i);
         bindings.put(c, i);
+        prefs.putInteger(controlToID(c), i);
+        prefs.flush();
+    }
+    public static String controlToID(Control c) {
+        switch (c) {
+            case GO_UP: return "up";
+            case GO_DOWN: return "down";
+            case GO_LEFT: return "left";
+            case GO_RIGHT: return "right";
+            case RESET_LEVEL: return "reset";
+            case SNEAKING: return "sneak";
+            case SPRINTING: return "sprint";
+            case INCREASE_LIGHT: return "incLightRad";
+            case DECREASE_LIGHT: return "decLightRad";
+        }
+        return null;
+    }
+    public static Control idToControl(String c) {
+        switch (c) {
+            case "up": return Control.GO_UP;
+            case "down": return Control.GO_DOWN;
+            case "left": return Control.GO_LEFT;
+            case "right": return Control.GO_RIGHT;
+            case "reset": return Control.RESET_LEVEL;
+            case "sneak": return Control.SNEAKING;
+            case "sprint": return Control.SPRINTING;
+            case "incLightrad": return Control.INCREASE_LIGHT;
+            case "decLightRad": return Control.DECREASE_LIGHT;
+        }
+        return null;
     }
     public static String controlToString(Control c) {
         switch (c) {
