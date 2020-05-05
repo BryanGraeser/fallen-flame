ae20ecbaee69a9b6f8a4d23789e2a9ba6bbfa8f3
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2020-05-05 23:34:53 +0800

    Add fade-in fade-out for text

--- core/src/com/fallenflame/game/TextController.java
+++ core/src/com/fallenflame/game/TextController.java
@@ -12,7 +12,11 @@ import java.util.*;
 
 public class TextController {
     private Map<Rectangle, String> texts;
-    private String nextMessage = null;
+    private List<String> nextMessage = null;
+    private float nextMessageAlpha;
+    private List<String> prevMessage = null;
+    private Rectangle nextMessageRect;
+    private float prevMessageAlpha;
     private BitmapFont displayFont;
     private int screenWidth;
 //    private int screenHeight;
@@ -28,54 +32,97 @@ public class TextController {
         }
         displayFont = JsonAssetManager.getInstance().getEntry("display", BitmapFont.class);
         screenWidth = Gdx.graphics.getWidth();
+        prevMessageAlpha = 1;
+        nextMessageAlpha = 0;
 //        screenHeight = Gdx.graphics.getHeight();
     }
 
     public void dispose() {
-        if (texts == null) return;
-        texts.clear();
-        texts = null;
+        if (texts != null) {
+            texts.clear();
+            texts = null;
+        }
+        if (nextMessage != null) {
+            nextMessage.clear();
+            nextMessage = null;
+        }
+        nextMessageRect = null;
     }
 
     public void update(PlayerModel player) {
         if (texts == null) return;
+        nextMessageAlpha = Math.min(nextMessageAlpha + .05f, 1);
+        prevMessageAlpha = Math.max(prevMessageAlpha - .05f, 0);
+        List<String> current = nextMessage;
+        Rectangle currentRect = nextMessageRect;
+        nextMessage = null;
+        nextMessageRect = null;
         for (Map.Entry<Rectangle, String> ele : texts.entrySet()) {
             if (ele.getKey().contains(player.getX(), player.getY())) {
-                nextMessage = ele.getValue();
-                return;
+                if (currentRect == ele.getKey()) {
+                    nextMessage = current;
+                    nextMessageRect = currentRect;
+                } else {
+                    prevMessage = current;
+                    nextMessage = splitLines(ele.getValue());
+                    nextMessageRect = ele.getKey();
+                    prevMessageAlpha = nextMessageAlpha;
+                    nextMessageAlpha = 0;
+                }
             }
         }
-        nextMessage = null;
+        if (nextMessage == null && current != null) {
+            prevMessage = current;
+            prevMessageAlpha = nextMessageAlpha;
+            nextMessageRect = null;
+            nextMessage = null;
+            nextMessageAlpha = 0;
+        }
+        if (prevMessage != null && prevMessageAlpha < 0.0001f) {
+            prevMessage.clear();
+            prevMessage = null;
+        }
+    }
+
+    private void renderText(GameCanvas canvas, Iterable<String> msg, float alpha) {
+        if (msg != null) {
+            displayFont.getData().setScale(.5f);
+            int i = 0;
+            displayFont.setColor(new Color(1,1,1, alpha));
+            for (String str : msg) {
+                canvas.drawTextFromCenter(str, displayFont, screenWidth / 2, 120 - i * 30);
+                i++;
+            }
+            displayFont.getData().setScale(1);
+        }
     }
 
     public void draw(GameCanvas canvas) {
-        if (texts == null || nextMessage == null) return;
+        if (texts == null || (prevMessage == null && nextMessage == null)) return;
         canvas.beginWithoutCamera();
-        displayFont.setColor(Color.WHITE);
-        displayFont.getData().setScale(.5f);
-        List<String> strs = splitLines(nextMessage);
-        int i = 0;
-        for (String str : strs) {
-            canvas.drawTextFromCenter(str, displayFont, screenWidth / 2, 120 - i * 30);
-            i ++;
-        }
-        displayFont.getData().setScale(1);
+        renderText(canvas, prevMessage, prevMessageAlpha);
+        renderText(canvas, nextMessage, nextMessageAlpha);
         canvas.end();
     }
 
-    private List<String> splitLines(String nextMessage) {
-        String[] texts = nextMessage.trim().split(" ");
+    private List<String> splitLines(String msg) {
+        String[] texts = msg.trim().split(" ");
         LinkedList<String> result = new LinkedList<>();
         result.add("");
         int start = 0;
         int end = 0;
         int all = texts.length;
+        displayFont.getData().setScale(.5f);
         while (true) {
             end++;
-            if (end > all) return result;
+            if (end > all) {
+                displayFont.getData().setScale(1);
+                return result;
+            }
             String[] currentLine = Arrays.copyOfRange(texts, start, end);
             String cl = String.join(" ", currentLine);
-            if (new GlyphLayout(displayFont, cl).width < screenWidth - 20) {
+            float pw = new GlyphLayout(displayFont, cl).width;
+            if (pw < screenWidth - 20) {
                 result.removeLast();
                 result.addLast(cl);
             } else {
