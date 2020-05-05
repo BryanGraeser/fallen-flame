d50efbcb76c9ce51f80e7157dcbe75f2466ad8fb
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2020-05-06 02:38:42 +0800

    Change where title is set

--- core/src/com/fallenflame/game/GDXRoot.java
+++ core/src/com/fallenflame/game/GDXRoot.java
@@ -60,7 +60,6 @@ public class GDXRoot extends Game implements ScreenListener {
 		engine.preLoadContent();
 		loading.setScreenListener(this);
 		setScreen(loading);
-		Gdx.graphics.setTitle("Fallen Flame");
 	}
 
 	/**
